1144ef1bede015d7e408e5f8ed643c4b9dd04ff5
eora21 <eora21@naver.com>
2025-02-19 15:46:32 +0900

    feat: 🎸 rabbitMQ 메시지에 따라 동작 처리하도록 코드 구성

--- src/main/java/org/example/compare/coupon/controller/CouponController.java
+++ src/main/java/org/example/compare/coupon/controller/CouponController.java
@@ -1,22 +1,33 @@
 package org.example.compare.coupon.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.compare.config.RabbitmqConfig;
 import org.example.compare.coupon.dto.CouponRequestDto;
 import org.example.compare.coupon.service.CouponService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/coupons")
 @RequiredArgsConstructor
 public class CouponController {
-
+    private final RabbitTemplate template;
     private final CouponService couponService;
 
     @PostMapping
-    public void wantCoupon(@RequestBody CouponRequestDto couponRequestDto) {
-        couponService.request(couponRequestDto);
+    public void requestCoupon(@RequestBody CouponRequestDto couponRequestDto) {
+        this.template.convertAndSend(RabbitmqConfig.QUEUE_NAME, couponRequestDto.accountId());
+    }
+
+    @RabbitListener(queues = RabbitmqConfig.QUEUE_NAME, concurrency = "5-10")
+    public void provideCoupon(Long accountId) {
+        UUID couponId = couponService.getCouponId();
+        couponService.provide(accountId, couponId);
     }
 }
--- src/main/java/org/example/compare/coupon/service/CouponService.java
+++ src/main/java/org/example/compare/coupon/service/CouponService.java
@@ -1,18 +1,69 @@
 package org.example.compare.coupon.service;
 
-import lombok.RequiredArgsConstructor;
-import org.example.compare.config.RabbitmqConfig;
-import org.example.compare.coupon.dto.CouponRequestDto;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.example.compare.coupon.entity.Coupon;
+import org.example.compare.coupon.repository.CouponRepository;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.data.redis.core.BoundListOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Service
-@RequiredArgsConstructor
 public class CouponService {
+    private final RedissonClient redissonClient;
+    private final CouponRepository couponRepository;
+    private final BoundListOperations<String, UUID> couponOps;
 
-    private final RabbitTemplate template;
+    public CouponService(RedisTemplate<String, UUID> redisTemplate, RedissonClient redissonClient, CouponRepository couponRepository) {
+        this.redissonClient = redissonClient;
+        this.couponRepository = couponRepository;
+        this.couponOps = redisTemplate.boundListOps("coupon");
+    }
 
-    public void request(CouponRequestDto request) {
-        this.template.convertAndSend(RabbitmqConfig.QUEUE_NAME, request);
+    public UUID getCouponId() {
+        return couponOps.getFirst();
+    }
+
+    @Transactional
+    public void provide(Long accountId, UUID couponId) {
+        if (Objects.isNull(couponId)) {
+            RLock lock = redissonClient.getLock("coupon");
+
+            try {
+                lock.tryLock(60, 5, TimeUnit.SECONDS);
+                insertCouponIdsInRedis(couponOps);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                lock.unlock();
+            }
+
+            couponId = couponOps.getFirst();
+        }
+
+        Coupon coupon = couponRepository.findById(couponId)
+                .orElseThrow();
+
+        coupon.setAccountId(accountId);
+    }
+
+    private void insertCouponIdsInRedis(BoundListOperations<String, UUID> couponOps) {
+        List<Coupon> notProvidedCoupons = couponRepository.findAllByAccountIdIsNull();
+
+        if (notProvidedCoupons.isEmpty()) {
+            return;
+        }
+
+        UUID[] notProvideCouponIds = notProvidedCoupons.stream()
+                .map(Coupon::getCouponCode)
+                .toArray(UUID[]::new);
+
+        couponOps.rightPushAll(notProvideCouponIds);
     }
 }
